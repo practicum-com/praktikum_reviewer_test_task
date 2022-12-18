6b76e47bb8ff98ef0ff10e28ce386a8b1bc41f90
fernandogutierrez <fernandogutierrez279@gmail.com>
2022-12-18 06:35:15 -0800

    Review student code and leaving comments

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import datetime as dt
 
 
 class Record:
+    # Create getter methods for date and amount
+
     def __init__(self, amount, comment, date=''):
+        # date parameter can contain a default value (today)
         self.amount = amount
+        # I suggest to create a new method to validate the date passed as parameter
         self.date = (
             dt.datetime.now().date() if
             not
@@ -19,44 +23,62 @@ class Calculator:
     def add_record(self, record):
         self.records.append(record)
 
+    # get_today_stats => we can create a new method inside Record class to check if a record was created Today
+    # instead of doing the calculations inside this method
     def get_today_stats(self):
         today_stats = 0
+        # Create getter methods for date and amount
+        # Record is a class not an object so this iteration will not work
         for Record in self.records:
             if Record.date == dt.datetime.now().date():
+                # this will not work, use limit variable instead to calculate 'today_stats'
                 today_stats = today_stats + Record.amount
         return today_stats
 
+    # get_week_stats => We can create a new method inside 'Record' class
+    # to check if a record belongs to the last 7 days
     def get_week_stats(self):
         week_stats = 0
         today = dt.datetime.now().date()
         for record in self.records:
+            # create variables, fix logic it is hard to read
             if (
-                (today - record.date).days < 7 and
-                (today - record.date).days >= 0
+                    (today - record.date).days < 7 and
+                    (today - record.date).days >= 0
             ):
                 week_stats += record.amount
         return week_stats
 
 
 class CaloriesCalculator(Calculator):
+    # Review Docstring Conventions to document methods
     def get_calories_remained(self):  # Gets the remaining calories for today
         x = self.limit - self.get_today_stats()
+        # x ?? create more meaningful variable names
         if x > 0:
+            # fix message:  'Today you can eat some...'
             return f'You can eat something else today,' \
                    f' but with a total calorie content of no more than {x} kcal'
         else:
-            return('Stop eating!')
+            # remove redundant parentheses
+            return ('Stop eating!')
 
 
 class CashCalculator(Calculator):
     USD_RATE = float(60)  # US dollar exchange rate.
     EURO_RATE = float(70)  # Euro exchange rate.
 
+    # parameter names should be lowercase
     def get_today_cash_remained(self, currency,
                                 USD_RATE=USD_RATE, EURO_RATE=EURO_RATE):
+        # redundant variable currency_type
         currency_type = currency
+
         cash_remained = self.limit - self.get_today_stats()
+        # Having too many if else statements makes it hard to read,
+        # create a dictionary instead to calculate cash_remained based on te currency_type
         if currency == 'usd':
+            # Create static variables for each currency
             cash_remained /= USD_RATE
             currency_type = 'USD'
         elif currency_type == 'eur':
@@ -71,11 +93,13 @@ class CashCalculator(Calculator):
                 f'{currency_type}'
             )
         elif cash_remained == 0:
+            # if it is 0 should return 'there is no money...'
             return 'No money, keep it up!'
         elif cash_remained < 0:
             return 'No money, keep it up:' \
                    ' your debt is - {0:.2f} {1}'.format(-cash_remained,
-                                                     currency_type)
+                                                        currency_type)
 
+    # Missing return statement
     def get_week_stats(self):
         super().get_week_stats()
